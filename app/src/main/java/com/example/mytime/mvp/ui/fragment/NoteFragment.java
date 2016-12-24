f2a047fc6b3d242d650d2435515ba46ee387646b
fenghao <593626601@qq.com>
2016-12-24 23:24:38 +0800

    主页面右上角计划和便签布局   主页面计划和便签fragment的布局以及recyclerview的item布局

---
+++ app/src/main/java/com/example/mytime/mvp/ui/fragment/NoteFragment.java
@@ -0,0 +1,25 @@
+package com.example.mytime.mvp.ui.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mytime.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NoteFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/mytime/mvp/ui/fragment/PlanFragment.java
@@ -0,0 +1,31 @@
+package com.example.mytime.mvp.ui.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mytime.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * 计划fragment
+ */
+public class PlanFragment extends Fragment {
+
+
+    public PlanFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plan, container, false);
+    }
+
+}
